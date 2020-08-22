223df3be36dde4afa1e49e7162703e6886b6603c
yashiagarwal786 <70066258+yashiagarwal786@users.noreply.github.com>
2020-08-22 20:31:35 +0530

    Add files via upload

---
+++ yashi.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 22 20:06:03 2020
+
+@author: DELL
+"""
+
+### PROBLEM STATEMENT-----> ####
+####   WRITE A PYTHON PROGRAM TO FIND THE GREATEST COMMMON DIVISOR(GCD) OF TWO INTEGERS
+
+def recurgcd(a,b):
+    low=min(a,b)
+    high=max(a,b)
+    
+    if low==0:
+        return high
+    elif low==1:
+        return 1
+    else:
+        return recurgcd(low,high%low)
+print(recurgcd(12, 14))
